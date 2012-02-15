dcaea1d8d0757d5c334c56aece40b6e55624f905
John Peterson <john.s.peterson@live.com>
2012-02-15 07:29:34 +0100

    Adding next P & S preset key
    Adding P & S preset OSD message

--- src/mpc-hc/MainFrm.h
+++ src/mpc-hc/MainFrm.h
@@ -718,6 +718,7 @@ public:
     afx_msg void OnViewAspectRatio(UINT nID);
     afx_msg void OnUpdateViewAspectRatio(CCmdUI* pCmdUI);
     afx_msg void OnViewAspectRatioNext();
+    afx_msg void OnViewPnSNext();
     afx_msg void OnViewOntop(UINT nID);
     afx_msg void OnUpdateViewOntop(CCmdUI* pCmdUI);
     afx_msg void OnViewOptions();
@@ -948,6 +949,7 @@ protected:
     void WTSUnRegisterSessionNotification();
 
     DWORD m_nMenuHideTick;
+    UINT m_iPnS;
     UINT m_nSeekDirection;
 public:
     afx_msg UINT OnPowerBroadcast(UINT nPowerEvent, UINT nEventData);
--- src/mpc-hc/resource.h
+++ src/mpc-hc/resource.h
@@ -1367,6 +1367,7 @@
 #define IDS_RESET_COLOR                 41319
 #define ID_MENU_LANGUAGE                41320
 #define ID_HELP_CHECKFORUPDATE          41321
+#define ID_PANSCAN_NEXT                 41322
 #define IDS_USING_LATEST_STABLE         41322
 #define IDS_USING_NEWER_VERSION         41323
 #define IDS_NEW_UPDATE_AVAILABLE        41324
@@ -1505,6 +1506,7 @@
 #define IDS_PPAGE_CAPTURE_SFG2          57353
 #define IDS_FILE_RECYCLE                57354
 #define IDS_FILE_DELETE                 57355
+#define IDS_AG_NEXT_PNS_PRESET          57356
 
 // Next default values for new objects
 // 
